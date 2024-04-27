311e14cc81376968c6e52d92bdb457c7c95a4bfe
imlampham <lam.pdp225546@sis.hust.edu.vn>
2024-04-27 22:51:56 +0700

    update

---
+++ OtherProjects/hust/soict/cybersecurity/garbage/GarbageCreator.java
@@ -0,0 +1,21 @@
+package hust.soict.cybersecurity.garbage;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class GarbageCreator {
+   public static void main(String[] args) throws IOException {
+		String filePath = "Documents/lampham.exe"; 
+		byte[] inputBytes = { 0 };
+		long startTime, endTime;
+		
+		inputBytes = Files.readAllBytes(Paths.get(filePath));
+		startTime = System.currentTimeMillis();
+		String outputString = "";
+		for(byte b : inputBytes) {
+			outputString += (char) b;
+		}
+		endTime = System.currentTimeMillis();
+		System.out.println(endTime - startTime);
+	}
+}
